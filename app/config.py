c13bbb4c6146ab693ed70ed00cbe2932415bec67
BANANANINJA <daniil757777@gmail.com>
2025-08-26 12:55:45 +0500

    a

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 QDRANT_HOST = os.getenv("QDRANT_HOST")
 QDRANT_API_KEY = os.getenv("QDRANT_API_KEY")              # <--- Укажите ваш Qdrant API ключ
-COLLECTION_NAME = "brooklyn_cloud_faq"
+COLLECTION_NAME = "bowling_knowledge"
 
 SERVICE_ACCOUNT_FILE = os.getenv("SERVICE_ACCOUNT_FILE")  # <--- Укажите путь к вашему JSON-ключу Google Service Account
 SPREADSHEET_ID = os.getenv("SPREADSHEET_ID")              # <--- Укажите ID вашей Google таблицы
